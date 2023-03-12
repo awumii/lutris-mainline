c1d0cfeb3d8133fd74fd0fc346d8e348114afa88
awumii <awumii@protonmail.com>
2023-03-12 20:05:51 +0100

    Support clickable URLs

--- lutris/gui/installer/widgets.py
+++ lutris/gui/installer/widgets.py
@@ -13,6 +13,11 @@ class InstallerLabel(Gtk.Label):
             self.set_property("ellipsize", Pango.EllipsizeMode.MIDDLE)
         self.set_alignment(0, 0.5)
         self.set_margin_right(12)
-        self.set_markup(text)
+
+        #Find and replace URLs with clickable links
+        url_regex = re.compile(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+        parsed_text = url_regex.sub(lambda url: f'<a href="{url.group()}">{url.group()}</a>', text)
+
+        self.set_markup(parsed_text)
         self.props.can_focus = False
         self.set_tooltip_text(text)
